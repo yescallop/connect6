95684b4da73ddc1ed4cc07e83ca2ab1de190ba00
yescallop <yescallop@users.noreply.github.com>
2022-12-19 14:17:23 +0000

    deploy: 136fe187f05d2b57ff3133b293dc05e927f9cce3

---
+++ implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"connect6":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"connect6/board/struct.Point.html\" title=\"struct connect6::board::Point\">Point</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
