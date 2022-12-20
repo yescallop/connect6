2f88e3e24978bf859a21b8ceab208692cf7f9291
yescallop <yescallop@users.noreply.github.com>
2022-12-20 04:19:27 +0000

    deploy: 96b78e130bca32ceb5ec7ba89d9b3c8eafa3980f

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"connect6":["",[["algorithm",[],["mcts.rs"]]],["algorithm.rs","board.rs","channel.rs","console.rs","lib.rs","message.rs","player.rs"]]\
+"connect6":["",[["algorithm",[],["mcts.rs","rng.rs"]]],["algorithm.rs","board.rs","channel.rs","console.rs","lib.rs","message.rs","player.rs"]]\
 }');
 createSourceSidebar();
